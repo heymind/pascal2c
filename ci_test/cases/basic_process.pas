30ecd0acf2507f754bd5f920b908396697dc9618
heymind <heymind@outlook.com>
2020-05-16 08:06:34 +0800

    test/basic_process: fix syntax error codegen: function decl var pass by ref codegen: emtpy return value

--- ci_test/cases/basic_process.pas
+++ ci_test/cases/basic_process.pas
@@ -1,12 +1,12 @@
 program basic_process(input,output);
 var a,b:integer;
-procedure swap(x,y:integer);
+procedure swap(var x,y:integer);
   var t:integer;
   begin
-    t:=x;x:=y;y:=t;
+    t:=x;x:=y;y:=t
   end;
 begin
   read(a,b);
   swap(a,b);
-  write('a=',a,'b=',b);
+  write(a,b)
 end.
