09aab624bcfe6e471e48eca7ccbbbebd5b33f16b
lightning1734 <34413787+lightning1734@users.noreply.github.com>
2020-05-16 19:48:47 +0800

    Update basic_const.pas

--- ci_test/cases/basic_const.pas
+++ ci_test/cases/basic_const.pas
@@ -1,7 +1,7 @@
 program basic_const(input,output);
 CONST char_type='d';
-CONST int_type='10';
-CONST real_type='1.6333';
+      int_type='10';
+      real_type='1.6333';
 begin
 write(char_type, ' ', int_type, ' ', real_type)
 end.
